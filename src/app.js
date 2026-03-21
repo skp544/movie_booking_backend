ef3d40c570bedfb44d3942eed132741d216605aa
skp544 <shubhamkumarprajapati544@gmail.com>
2026-03-21 11:55:10 +0530

    Add helmet middleware for enhanced security in the application

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const movieRouter = require("./routes/movie.routes");
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Hello from the movie booking backend" });
