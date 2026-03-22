b59aed48c6458fcbf1360d714113dc091c42458e
skp544 <shubhamkumarprajapati544@gmail.com>
2026-03-22 19:08:26 +0530

    Refactor theatre creation logic: enhance error handling and add middleware file

--- src/controllers/theatre.controller.js
+++ src/controllers/theatre.controller.js
@@ -7,11 +7,13 @@ const {
 exports.create = async (req, res) => {
   try {
     const response = await theatreService.createTheatre(req.body);
-    successResponseBody.data = response;
-    successResponseBody.message = "Successfully created theatre.";
 
     return res.status(201).json(successResponseBody);
   } catch (error) {
+    if (error.err) {
+      errorResponseBody.err = error.err;
+      return res.status(error.code).json(errorResponseBody);
+    }
     errorResponseBody.err = error;
     return res.status(500).json(errorResponseBody);
   }
--- src/services/theatre.service.js
+++ src/services/theatre.service.js
@@ -4,7 +4,15 @@ exports.createTheatre = async (data) => {
   try {
     const response = await TheatreModel.create(data);
     return response;
-  } catch (err) {
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      let err = {};
+      Object.keys(error.errors).forEach((key) => {
+        err[key] = error.errors[key].message;
+      });
+
+      throw { err, code: 422 };
+    }
     throw err;
   }
 };
